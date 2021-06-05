41a5dd8c3d864004bc656df53d0e63b5c74cb3f8
MelbouVP <saurfanng@gmail.com>
2021-06-06 00:05:46 +0300

    Implemented User story 6 - After triangle is calculated, it is drawn in the drawing window

--- src/Components/Drawing/drawing.component.jsx
+++ src/Components/Drawing/drawing.component.jsx
@@ -6,6 +6,88 @@ function Drawing({ isDrawable, triangleSides }) {
 
     const canvasRef = useRef(null)
 
+    const determineFactor = useCallback((sideC) => {
+        
+        if(sideC < 5) return 120
+        if(sideC < 10) return 58
+        if(sideC < 16) return 30
+        if(sideC < 22) return 22
+        if(sideC < 35) return 14
+        if(sideC < 50) return 8
+        if(sideC < 75) return 7
+        if(sideC < 101) return 5    
+
+        },[]
+    )
+
+  
+    useEffect(() => {
+
+        // if triangle is not possible to draw, don't start calculation
+        if(!isDrawable) return
+
+        // sort sides from smallest to biggest
+        let [sideA, sideB, sideC] = [triangleSides.sideA, triangleSides.sideB, triangleSides.sideC].sort((a, b) => a-b);
+
+        // canvas size is equal to canvas html element
+        let canvasWidth = 600
+        let canvasHeight = 500
+
+        // initialize canvas
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        ctx.lineWidth = 3;
+        
+        // clean previous drawings
+        ctx.clearRect(0, 0, 600, 500);
+
+
+        // factor in different side sizes in order to fit in the canvas 
+        // the smaller the sides the bigger the factor and vice versa 
+        let factor = determineFactor(sideC)
+
+
+        // Calculate triangle area using Heron's formula 
+        let area = 0.25 * Math.sqrt((sideA+sideB+sideC) * (-sideA+sideB+sideC)*(sideA-sideB+sideC)*(sideA+sideB-sideC)) 
+        
+        // Calculate triangle height using triangle height formula 
+        let height = ((2 * area) / sideC).toPrecision(2); 
+
+
+        // Slope of base is the distance from side A starting point to heighest point of triangle
+        // it is calculated using Pythogorean theorem (d = sqrt( a^2 + b^2 )  ) 
+        // side A is taken to ensure that if triangle is isoceles then drawing is symetrical (base will be the shortest side)
+        let slopeOfBase = Math.sqrt(Math.pow(sideA, 2) - Math.pow(height, 2)).toPrecision(2); 
+
+        // Canva starting X coordinate is middle of canva width minus half of side C length 
+        let initialPointX = canvasWidth / 2 - 0.5 * sideC * factor 
+
+        // Canva starting Y coordinate is middle of canva height plus half of triangle height 
+        // (because canva Y axis go in other direction) 
+        let initialPointY = canvasHeight / 2 + 0.5 * height * factor 
+        
+        ctx.beginPath(); 
+        // Move canva pen to canva starting coordinates 
+        ctx.moveTo( initialPointX,  initialPointY); 
+
+        // Draw triangle base
+        // X coordinate is middle of canva width plus half of side C length
+        // Y coordinate is middle of canva height plus half of triangle height   
+        ctx.lineTo( (canvasWidth / 2 + 0.5 * sideC * factor), (canvasHeight/2 + 0.5 * height * factor))
+
+        // Draw right side of triangle
+        // X coordinate is middle of canva width plus half of side C length minus the distance of the slope
+        // Y coordinate is middle of canva height minus half of triangle height 
+        ctx.lineTo((canvasWidth / 2 + 0.5 * sideC * factor - slopeOfBase * factor), (canvasHeight/2 - 0.5 * height * factor));
+
+
+        // Draw left side of triangle
+        ctx.lineTo(initialPointX, initialPointY); 
+        
+        ctx.stroke();
+                        
+        
+    }, [isDrawable,triangleSides, determineFactor])
 
     return (
         <div className="drawing">
